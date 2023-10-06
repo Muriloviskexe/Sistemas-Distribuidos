992480c30c7d9505b2e7be29a971f524a06200f6
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-10-05 22:17:02 -0300

    Aula 04 de SD

--- Teorica/Aula04/cliente/Cliente.java
+++ Teorica/Aula04/cliente/Cliente.java
@@ -1,28 +1,71 @@
 package Teorica.Aula04.cliente;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Cliente{
+public class Cliente {
+//* Cria o main do java
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
+        //Variaveis
         Socket socket;
+
+        //define a porta de comunicação
         final int PORT = 12345;
-        final String IP = "localhost";
+        final String IP = "127.0.0.1"; // localhost
 
-    //Criar Socket e solicitar conexão
-    try {
-        socket = new Socket(IP, PORT);
-        System.out.println("Conectado com sucesso!!");
-    } catch (Exception e) {
-        System.out.println("Erro de conexão");
-        return;
-    }
-    //Etapa de comunicação
 
-    //Encerrar conexão
-    try {
-        socket.close();
-    } catch (Exception e) {
-        
+        //output envia a mensagem
+        PrintStream output;
+        Scanner input;
+
+        //* criar o socket e solicita a conexão
+        try {
+
+            //Faz a conexão de socket do client com o serve
+            socket = new Socket(IP, PORT);
+            output = new PrintStream(socket.getOutputStream());
+
+            //InputStream faz o recebimento de comunicação
+            input = new Scanner(socket.getInputStream());
+            //Menssagem de confirmação
+            System.out.println("Conectado com sucesso!");
+
+        } catch (Exception e) {
+            System.out.println("Erro na conexão");
+            return;
+        }
+
+        //* etapa de comunicação
+        try {
+            Scanner teclado = new Scanner(System.in);
+            String msg, tranlate;
+            do {
+                System.out.println("Digite uma cor:");
+                //grava o que foi escrito no console
+                msg = teclado.nextLine();
+
+                //output envia a mensagem
+                output.println(msg);
+
+                //Lê o que recebeu do servidor
+                tranlate = input.nextLine();
+                System.out.println("Tradução: " + tranlate);
+
+            } while(!msg.equals("exit"));//Enquanto a mensagem não for 'exit' não fecha a comunicação
+
+        } catch (Exception e) {
+            System.out.println("Erro ao enviar os dados");
+        }
+
+        //* encerrar a conexão
+        try {
+            
+            //encerra o socket
+            socket.close();
+        } catch (Exception e) {
+            
+        }
     }
-}}
+}
--- Teorica/Aula04/server/Server.java
+++ Teorica/Aula04/server/Server.java
@@ -1,43 +1,93 @@
 package Teorica.Aula04.server;
 
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Server {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ServerSocket serverSocket;
-        final int PORT = 12345;
         Socket socketClient;
 
-    //Criar o ServerSocket e fazer o bind
-    try{
-        serverSocket = new ServerSocket(PORT);
-        System.out.println("Bind Sucess");
-    } catch (Exception e) {
-        System.out.println("Erro: Porta em uso");
-        return;
-    }
+        //define a porta de comunicação
+        final int PORT = 12345;
+        Scanner input;
 
-    //Aguardar pedido de conexão
-    try{
-        System.out.println("Esperando Cliente....");
-        socketClient = serverSocket.accept();
-        System.out.println("Conexão bem sucedida com " + socketClient.getInetAddress());
-    } 
-        catch (Exception e){
-            System.out.println("Erro: Conexão falhou");
+        //output envia a mensagem
+        PrintStream output;
+
+        HashMap<String, String> dictionary = new HashMap<>();
+
+        dictionary.put("red", "Vermelho");
+        dictionary.put("green", "Verde");
+        dictionary.put("white", "Branco");
+        dictionary.put("black", "Preto");
+        dictionary.put("purple", "Roxo");
+        dictionary.put("gray", "Cinza");
+        dictionary.put("blue", "Azul");
+
+    // criar o ServerSocket e fazer o bind
+        try {
+
+            //Cria um socket para comunicação
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Bind success!");
+
+        } catch (Exception e) {
+            System.out.println("Erro: Porta em uso");
             return;
-    }
-    
-    //Etapa de comunicação
+        }
 
-    
-    //Encerrar conexão
-    try {
-        socketClient.close();
-        serverSocket.close();
-    } catch (Exception e) {
-        
+        // aguardar pedido de conexão
+        try {
+            System.out.println("Esperando um cliente...");
+
+            //Fica aguardando uma mensagem do cliente, até lá nada acontece
+            socketClient = serverSocket.accept();
+
+            //input lê a mensagem recebida
+            input = new Scanner(socketClient.getInputStream());
+
+            //output vai mandar uma mensagem de volta
+            output = new PrintStream(socketClient.getOutputStream());
+
+            System.out.println("Conectado com " + socketClient.getInetAddress());
+
+
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar com o cliente");
+            return;
+        }
+
+        // etapa de comunicação
+        try {
+            String msg;
+            do {
+
+                //lê a mensagem recebida
+                msg = input.nextLine();
+                System.out.println("Recebido: " + msg);
+                
+                //encaminha a mensagem de acordo com o correspontente no dicionario
+                output.println(dictionary.get(msg));
+
+            } while (!msg.equals("exit")); //não encerra enquanto não receber a mensagem certa
+
+        } catch (Exception e) {
+            System.out.println("Erro ao receber os dados");
+        }
+
+        // encerrar a conexão
+        try {
+
+            //encerra os dois sockets
+            socketClient.close();
+            serverSocket.close();
+        } catch (Exception e) {
+
+        }
     }
-}}
+}
