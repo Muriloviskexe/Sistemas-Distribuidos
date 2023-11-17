87e3177dac7582372c98697722ad06a25f58e2ca
Murilo <murilogccontato@gmail.com>
2023-11-17 19:27:58 -0300

    subindo servidor

--- Pratica/Projeto-Micro_serviços/classificacao/index.js
+++ Pratica/Projeto-Micro_serviços/classificacao/index.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
+//const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-app.post('/eventos', (req, res) =>{
+app.post("/eventos", (req, res) => {
+
 });
 
-app.listen(7000, () => console.log ("Classificação. Porta 7000"));
+app.listen(7000, () => console.log("Classificação. Porta 7000."));
